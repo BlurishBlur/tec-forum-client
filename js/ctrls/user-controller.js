d203db7e1ae952ee84bc8b4dcfbc082ecf6fb6c3
Niels Heltner <niels.heltner@hotmail.com>
2017-10-22 15:50:08 +0200

    ryddet op i time conversion

--- js/ctrls/user-controller.js
+++ js/ctrls/user-controller.js
@@ -1,6 +1,14 @@
 /*global angular*/
 /*jslint browser: true*/
 /*global $, jQuery, alert*/
+var units = { // conversion from seconds to other units
+    "minute": 60, 
+    "hour": 60 * 60, 
+    "day": 60 * 60 * 24, 
+    "week": 60 * 60 * 24 * 7, 
+    "month": 60 * 60 * 24 * 7 * 4, 
+    "year": 60 * 60 * 24 * 7 * 4 * 12
+}
 
 angular.module('forumApp').controller('userCtrl', function ($scope, $location, $routeParams) {
     "use strict";
@@ -14,42 +22,21 @@ angular.module('forumApp').controller('userCtrl', function ($scope, $location, $
     function getUserThreads () {
         getWithParams(getUrl('/users/threads'), $routeParams.id, function (content) {
             $scope.threadsDTO = JSON.parse(content);
-            for(var i = 0; i < $scope.threadsDTO.length; i++) { // skal gøres pænere
-                var creationDate = new Date($scope.threadsDTO[i].creationDate);
-                var now = new Date();
-                var difference = (now - creationDate) / (1000);
+
+            for(var i = 0; i < $scope.threadsDTO.length; i++) {
+                var difference = (new Date() - new Date($scope.threadsDTO[i].creationDate)) / (1000); // miliseconds --> seconds
                 var factor = 1;
                 var notation = 'seconds';
-                if(difference > 60 - 1) { // sekunder --> minutter
-                    factor *= 60;
-                    notation = 'minutes';
-                }
-                if(difference > (60 * 60) -1) { // minutter --> timer
-                    factor *= 60;
-                    notation = 'hours';
-                }
-                if(difference > (60 * 60 * 24) - 1) { //timer --> dage
-                    factor *= 24;
-                    notation = 'days';
-                }
-                if(difference > (60 * 60 * 24 * 7) - 1) { //dage --> uger
-                    factor *= 7;
-                    notation = 'weeks';
-                }
-                if(difference > (60 * 60 * 24 * 7 * 4) - 1) { // uger --> måneder
-                    factor *= 4;
-                    notation = 'months';
-                }
-                if(difference > (60 * 60 * 24 * 7 * 4 * 12) - 1) { // måneder --> år
-                    factor *= 12;
-                    notation = 'years';
+
+                for(var unit in units) {
+                    if(difference > (units[unit] - 1)) {
+                        factor = units[unit];
+                        notation = unit;
+                    }
                 }
                 $scope.threadsDTO[i].difference = Math.round(difference / factor);
-                $scope.threadsDTO[i].notation = notation;
+                $scope.threadsDTO[i].notation = ($scope.threadsDTO[i].difference > 1 ? notation + 's' : notation);
             }
-            //console.log(creationDate);
-
-            //console.log($scope.threadsDTO);
 
             $scope.$apply();
         });
@@ -58,6 +45,7 @@ angular.module('forumApp').controller('userCtrl', function ($scope, $location, $
     function getUser () {
         getWithParams(getUrl('/users'), $routeParams.id, function (content) {
             $scope.userDTO = JSON.parse(content);
+
             var date = new Date($scope.userDTO.creationDate);
             $scope.userDTO.creationDate = date.getDate() + ' ' + date.toLocaleString('en-US', { month: "long" }) + ' ' + date.getFullYear();
 
