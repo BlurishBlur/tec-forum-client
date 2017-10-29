bfeb8b7839f7020097ccbc21056b776813de7bce
Niels Heltner <niels.heltner@hotmail.com>
2017-10-29 20:51:00 +0100

    flyttet http metoder ind i router, og reused mere kode

--- js/main.js
+++ js/main.js
@@ -21,54 +21,6 @@ function colorBorderGrey(inputElement) {
     inputElement.css("border", "3px solid #9EA9AB");
 }
 
-function get(url, callback) {
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.onreadystatechange = function() {
-        if (xmlHttp.readyState === 4 && xmlHttp.status === 200) {
-            callback(xmlHttp.responseText);
-        }
-    };
-    xmlHttp.onerror = function(error) {
-        console.log('Error connecting to server.');
-    }
-    xmlHttp.open("GET", url, true); // true for asynchronous 
-    xmlHttp.send(null);
-}
-
-function getWithParams(url, data, callback) {
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.onreadystatechange = function() {
-        if (xmlHttp.readyState === 4 && xmlHttp.status === 200) {
-            callback(xmlHttp.responseText);
-        }
-    };
-    xmlHttp.open("GET", url + '?id=' + data, true); // true for asynchronous 
-    xmlHttp.send(null);
-}
-
-function put(url, data, callback) {
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.onreadystatechange = function() {
-        if (xmlHttp.readyState === 4 && xmlHttp.status === 200) {
-            callback(xmlHttp.responseText);
-        }
-    };
-    xmlHttp.open("PUT", url, true); // true for asynchronous 
-    xmlHttp.send(data);
-}
-
-function post(url, data, callback) {
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.onreadystatechange = function() {
-        if (xmlHttp.readyState === 4 && xmlHttp.status === 200) {
-            callback(xmlHttp.responseText);
-        }
-    };
-    xmlHttp.open("POST", url, true); // true for asynchronous 
-    xmlHttp.send(data);
-}
-
-
 /*
 angular.module('forumApp').filter('debug', function() {
   return function(input) {
